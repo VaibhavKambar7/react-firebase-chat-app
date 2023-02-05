93e36c0ca54419ddac1ad24c46ce361ea4dacb42
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-02-05 20:37:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Auth } from "./components/Auth";
+import React, { useState, useRef } from "react";
+import { Chat } from "./components/Chat";
+import Cookies from "universal-cookie";
+import { async } from "@firebase/util";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+const cookies = new Cookies();
 
 function App() {
+  const [isAuth, setisAuth] = useState(cookies.get("auth-token")); //imported cookies for this
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setisAuth(null);
+    setRoom(null);
+  };
+
+  if (!isAuth) {
+    return (
+      <div className="App">
+        <Auth setisAuth={setisAuth} />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {room ? (
+        <Chat room={room} />
+      ) : (
+        <div className="room">
+          <div className="label">
+            <label className="room1">Enter Room Number</label>
+          </div>
+          <div className="input">
+            {/* <input onChange={(e) => setRoom(e.target.value)}/> */}
+            <input ref={roomInputRef} />
+          </div>
+          <div className="button">
+            <button
+              className="enter_chat_button"
+              onClick={() => setRoom(roomInputRef.current.value)}
+            >
+              Enter Chat
+            </button>
+          </div>
+        </div>
+      )}
+
+      <div className="sign_out">
+        <button className="sign_out_button" onClick={signUserOut}>
+          Sign Out
+        </button>
+      </div>
+    </>
   );
 }
 
